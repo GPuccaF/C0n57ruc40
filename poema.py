c190fb12e5e042411da3e091ec52f666a90986f6
GPuccaF <xavantg@gmail.com>
2016-03-25 16:06:59 -0300

    Added files via upload

---
+++ poema.py
@@ -0,0 +1,92 @@
+# C0n2truç40
+from random import randint
+def main():
+    while "Construção" == "Construção":
+        props = ["a última", "o último", "máquina"]
+        prop = props[randint(0, 2)]
+        verso = ("Amou daquela vez como se fosse %s " % prop)
+        print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["a última", "a única", "lógico"]
+            prop = props[randint(0, 2)]
+            verso = ("Beijou sua mulher como se fosse %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["único", "pródigo"]
+            prop = props[randint(0, 1)]
+            verso = ("E cada filho seu como se fosse o %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["tímido", "bêbado"]
+            prop = props[randint(0, 1)]
+            verso = ("E atravessou a rua com seu passo %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["máquina", "sólido"]
+            prop = props[randint(0, 1)]
+            verso = ("Subiu a construção como se fosse %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["sólidas", "mágicas", "flácidas"]
+            prop = props[randint(0, 2)]
+            verso = ("Ergueu no patamar quatro paredes %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["mágico", "lógico"]
+            prop = props[randint(0, 1)]
+            verso = ("Tijolo com tijolo num desenho %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["lágrima", "tráfego"]
+            prop = props[randint(0, 1)]
+            verso = ("Seus olhos embotados de cimento e %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["sábado", "um príncipe", "um pássaro"]
+            prop = props[randint(0, 2)]
+            verso = ("Sentou pra descansar como se fosse %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["um príncipe", "o máximo"]
+            prop = props[randint(0, 1)]
+            verso = ("Comeu feijão com arroz como se fosse %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["um náufrago", "máquina"]
+            prop = props[randint(0, 1)]
+            verso = ("Bebeu e soluçõu como se fosse %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["ouvisse música", "fosse o próximo"]
+            prop = props[randint(0, 1)]
+            verso = ("Dançou e gargalhou como se %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["ouvisse música", "fosse um bêbado"]
+            prop = props[randint(0, 1)]
+            verso = ("E tropeçou no céu como se %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["um pássaro", "sábado", "um príncipe"]
+            prop = props[randint(0, 2)]
+            verso = ("E flutuou no ar como se fosse %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["flácido", "tímido", "bêbado"]
+            prop = props[randint(0, 2)]
+            verso = ("E se acabou no chão feito um pacote %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["público", "náufrago"]
+            prop = props[randint(0, 1)]
+            verso = ("Agonizou no meio do passeio %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            props = ["tráfego", "público", "sábado"]
+            prop = props[randint(0, 2)]
+            verso = ("Morreu na contramão atrapalhando o %s " % prop)
+            print(verso)
+        if verso[len(verso) - 1] == " ":
+            proximo = " "
+            print(proximo)
+main()
